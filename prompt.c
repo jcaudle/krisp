5fa12651c9225f5cf34ad4883215fa18c5b2384c
Joseph Caudle <joseph@josephcaudle.com>
2016-10-15 22:54:19 -0400

    Support multiple environments

--- prompt.c
+++ prompt.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compiling on WIndows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+/* I think this will only work on Mac */
+#else
 #include <editline/readline.h>
+/* Linux likely needs editline/history.h */
+#endif
 
 int main(int argc, char** argv) {
   /* Print Version and Exit Information */
-  puts("Krisp Version 0.2.0");
+  puts("Krisp Version 0.2.1");
   puts("Press Ctrl+c to Exit\n");
 
   /* In a never ending loop */
