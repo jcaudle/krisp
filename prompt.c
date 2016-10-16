ad354c9b23b5ceda9a943e98b138a152976a7fb3
Joseph Caudle <joseph@josephcaudle.com>
2016-10-15 22:48:41 -0400

    Add ability to edit lines

--- prompt.c
+++ prompt.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
   /* Print Version and Exit Information */
-  puts("Krisp Version 0.1.0");
+  puts("Krisp Version 0.2.0");
   puts("Press Ctrl+c to Exit\n");
 
   /* In a never ending loop */
   while (1) {
-    /* Output our prompt */
-    fputs("krisp> ", stdout);
+    /* Output our prompt and get input */
+    char* input = readline("krisp> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
